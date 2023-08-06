acba71e7b193161c5450eb794e1f10d16d9498c4
carlosquadros-br <carlos.quadros.br@gmail.com>
2023-08-06 08:50:07 -0300

    add tests

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,57 +1,64 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { User } from './schemas/user.schema';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import * as bycript from 'bcryptjs';
-import { JwtService } from '@nestjs/jwt';
-import { singUpDto } from './dto/singup.dto';
-import { LoginDto } from './dto/login.dto';
-import * as bcrypt from 'bcryptjs';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { User } from "./schemas/user.schema";
+
+import * as bcrypt from "bcryptjs";
+import { JwtService } from "@nestjs/jwt";
+import { LoginDto } from "./dto/login.dto";
+import { SignUpDto } from "./dto/signup.dto";
 
 @Injectable()
 export class AuthService {
-    constructor(
-        @InjectModel(User.name)
-        private userModel: Model<User>,
-        private jwtService: JwtService
-    ) {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+    private jwtService: JwtService
+  ) {}
 
-    }
+  async signUp(signUpDto: SignUpDto): Promise<{ token: string }> {
+    const { name, email, password } = signUpDto;
 
-    async signUp(singUpDto: singUpDto): Promise<{ token: string}> {
-        const { name, email, password } = singUpDto
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        const hashedPassword = await bycript.hash(password, 10);
+    try {
+      const user = await this.userModel.create({
+        name,
+        email,
+        password: hashedPassword,
+      });
 
-        const user = await this.userModel.create(
-            {
-                name,
-                email,
-                password: hashedPassword
-            }
-        )
+      const token = this.jwtService.sign({ id: user._id });
 
-        const token = this.jwtService.sign({ id: user._id })
-
-        return { token };
-    }
-    async login(loginDto: LoginDto): Promise<{ token: string }> {
-        const { email, password } = loginDto;
-    
-        const user = await this.userModel.findOne({ email });
-    
-        if (!user) {
-          throw new UnauthorizedException('Invalid email or password');
-        }
-    
-        const isPasswordMatched = await bcrypt.compare(password, user.password);
-    
-        if (!isPasswordMatched) {
-          throw new UnauthorizedException('Invalid email or password');
-        }
-    
-        const token = this.jwtService.sign({ id: user._id });
-    
-        return { token };
+      return { token };
+    } catch (error) {
+      if (error?.code === 11000) {
+        throw new ConflictException("Duplicate Email Entered");
       }
+    }
+  }
+
+  async login(loginDto: LoginDto): Promise<{ token: string }> {
+    const { email, password } = loginDto;
+
+    const user = await this.userModel.findOne({ email });
+
+    if (!user) {
+      throw new UnauthorizedException("Invalid email or password");
+    }
+
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatched) {
+      throw new UnauthorizedException("Invalid email or password");
+    }
+
+    const token = this.jwtService.sign({ id: user._id });
+
+    return { token };
+  }
 }
--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,6 +1,6 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 
-export class singUpDto {
+export class SignUpDto {
     @IsNotEmpty()
     @IsString()
     readonly name: string;
