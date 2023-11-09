9d05749489ec19c5091759d84a9641d684f0cbe7
Rafhael369 <rafhael369@gmail.com>
2023-11-09 09:24:04 -0300

    Adicionado auto-reload

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   // mudar para funcionar no front-end
-  await app.listen(3000, '192.168.0.27');
+  await app.listen(3000);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
